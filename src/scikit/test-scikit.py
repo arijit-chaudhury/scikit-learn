d445c8285e51698c9fdf88eb92b254f31f05147b
ARIJIT CHAUDHURY <34763856+arijit-chaudhury@users.noreply.github.com>
2024-12-26 22:17:48 +0000

    First scikit prediction

--- src/scikit/test-scikit.py
+++ src/scikit/test-scikit.py
@@ -1,1 +1,10 @@
-import sklearn
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.datasets import load_breast_cancer
+
+X, y = load_breast_cancer(return_X_y=True)
+print(X)
+#print(y)
+
+model = KNeighborsRegressor().fit(X, y)
+predict_data = model.predict(X)
+print(predict_data)
