03a8df746d34a958015e877aa47fd4a2798bff34
ARIJIT CHAUDHURY <34763856+arijit-chaudhury@users.noreply.github.com>
2024-12-26 23:52:56 +0000

    First scikit prediction

--- src/scikit/testscikit.py
+++ src/scikit/testscikit.py
@@ -1,17 +1,20 @@
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.datasets import load_breast_cancer
+import matplotlib.pyplot as plt
 
 
 class Data:
     def __init__(self):
         print('Init')
 
-    def learn_data(self):
+    def learn_data (self):
         X, y = load_breast_cancer(return_X_y=True)
         print(X)
         model = KNeighborsRegressor().fit(X, y)
         predict_data = model.predict(X)
         print(predict_data)
+        plt.scatter(predict_data, y)
+        plt.show()
 
 
 if __name__ == '__main__':
